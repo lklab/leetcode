2f766f449223f59ca015d4a67136bd3e28c27249
lklab <alexeibatic@gmail.com>
2024-10-19 20:43:53 +0900

    Time: 0 ms (100%), Space: 7.4 MB (64.16%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
@@ -3,32 +3,22 @@ class Solution
 public:
     char findKthBit(int n, int k)
     {
-        n = 0;
-        int temp = k;
-        while (temp > 0)
-        {
-            ++n;
-            temp >>= 1;
-        }
-
-        int arr[(1 << n) - 1];
-        arr[0] = 0;
-        int currentSize = 1;
-
-        for (int i = 1; i < n; ++i)
-        {
-            arr[currentSize] = 1;
-            int nextSize = (currentSize << 1) + 1;
-            for (int j = 1; j <= currentSize; ++j)
-            {
-                arr[currentSize + j] = !arr[currentSize - j];
-            }
-            currentSize = nextSize;
-        }
-
-        if (arr[k - 1] == 0)
-            return '0';
-        else
-            return '1';
+        // Base case: When n = 1, the binary string is "0"
+        if (n == 1) return '0';
+        
+        // Find the length of the current string Sn, which is 2^n - 1
+        int length = (1 << n) - 1;
+        
+        // Find the middle position
+        int mid = length / 2 + 1;
+        
+        // If k is the middle position, return '1'
+        if (k == mid) return '1';
+        
+        // If k is in the first half, find the bit in Sn-1
+        if (k < mid) return findKthBit(n - 1, k);
+        
+        // If k is in the second half, find the bit in Sn-1 and invert it
+        return findKthBit(n - 1, length - k + 1) == '0' ? '1' : '0';
     }
 };
