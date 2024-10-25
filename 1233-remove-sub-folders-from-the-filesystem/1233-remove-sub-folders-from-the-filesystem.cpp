1bb312cec3b1a77cf2d9a174e08a32da2c1df18f
lklab <alexeibatic@gmail.com>
2024-10-25 15:14:28 +0900

    Time: 46 ms (95.88%), Space: 45.7 MB (87.36%) - LeetHub

---
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+    vector<string> removeSubfolders(vector<string>& folder)
+    {
+        sort(folder.begin(), folder.end());
+
+        vector<string> result;
+        result.push_back(folder[0]);
+        int last = 0;
+
+        for (int i = 1; i < folder.size(); ++i)
+        {
+            string lastFolder = result[last] + '/';
+
+            if (folder[i].compare(0, lastFolder.size(), lastFolder) != 0)
+            {
+                result.push_back(folder[i]);
+                ++last;
+            }
+        }
+
+        return result;
+    }
+};
