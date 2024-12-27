a810eba7f79d9904705c8f9d30bcdc3f0fec33af
lklab <alexeibatic@gmail.com>
2024-12-27 10:31:26 +0900

    Time: 0 ms (100%), Space: 45.5 MB (55.58%) - LeetHub

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.cpp
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int maxScoreSightseeingPair(vector<int>& values) {
         int result = -2147483648;
-        int m = values[0];
+        int left = values[0];
 
         for (int i = 1; i < values.size(); ++i) {
-            if (values[i] - i + m > result) {
-                result = values[i] - i + m;
+            if (values[i] - i + left > result) {
+                result = values[i] - i + left;
             }
 
-            if (values[i] + i > m) {
-                m = values[i] + i;
+            if (values[i] + i > left) {
+                left = values[i] + i;
             }
         }
 
