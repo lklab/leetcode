75a98058075c790215bc6ae71563920036d8eb24
lklab <alexeibatic@gmail.com>
2025-01-08 14:26:43 +0900

    Time: 1 ms (76.69%), Space: 24.2 MB (30.51%) - LeetHub

--- 3042-count-prefix-and-suffix-pairs-i/3042-count-prefix-and-suffix-pairs-i.cpp
+++ 3042-count-prefix-and-suffix-pairs-i/3042-count-prefix-and-suffix-pairs-i.cpp
@@ -1,91 +1,24 @@
-class TrieNode {
-public:
-    unordered_set<int> contains;
-    unordered_map<char, TrieNode> next;
-
-    TrieNode* append(int index, char c) {
-        if (next.find(c) == next.end()) {
-            next[c] = TrieNode();
-        }
-
-        next[c].contains.insert(index);
-        return &next[c];
-    }
-};
-
 class Solution {
 private:
-    unordered_set<int>* countPrefix(string& word, TrieNode* prefixRoot) {
-        TrieNode* current = prefixRoot;
-
-        for (int i = 0; i < word.size(); ++i) {
-            if (current -> next.find(word[i]) == current -> next.end()) {
-                return nullptr;
-            } else {
-                current = &(current -> next[word[i]]);
-            }
-        }
-
-        return &(current -> contains);
-    }
-
-    unordered_set<int>* countSuffix(string& word, TrieNode* suffixRoot) {
-        TrieNode* current = suffixRoot;
-
-        for (int i = word.size() - 1; i >= 0; --i) {
-            if (current -> next.find(word[i]) == current -> next.end()) {
-                return nullptr;
-            } else {
-                current = &(current -> next[word[i]]);
-            }
-        }
-
-        return &(current -> contains);
-    }
-
-    void appendPrefix(int index, string& word, TrieNode* prefixRoot) {
-        TrieNode* current = prefixRoot;
-
-        for (int i = 0; i < word.size(); ++i) {
-            current = current -> append(index, word[i]);
-        }
-    }
-
-    void appendSuffix(int index, string& word, TrieNode* suffixRoot) {
-        TrieNode* current = suffixRoot;
-
-        for (int i = word.size() - 1; i >= 0; --i) {
-            current = current -> append(index, word[i]);
-        }
+    bool isPrefixAndSuffix(const string& str1, const string& str2) {
+        int len1 = str1.size(), len2 = str2.size();
+        if (len1 > len2) return false;
+        return str1 == str2.substr(0, len1) && str1 == str2.substr(len2 - len1, len1);
     }
 
 public:
     int countPrefixSuffixPairs(vector<string>& words) {
-        TrieNode prefixRoot;
-        TrieNode suffixRoot;
+        int n = words.size();
+        int count = 0;
 
-        int result = 0;
-
-        for (int i = words.size() - 1; i >= 0; --i) {
-            unordered_set<int>* prefixSet = countPrefix(words[i], &prefixRoot);
-            unordered_set<int>* suffixSet = countSuffix(words[i], &suffixRoot);
-
-            if (prefixSet != nullptr && suffixSet != nullptr) {
-                unordered_set<int> intersectionSet;
-
-                for (const auto& index : *prefixSet) {
-                    if (suffixSet -> find(index) != suffixSet -> end()) {
-                        intersectionSet.insert(index);
-                    }
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                if (isPrefixAndSuffix(words[i], words[j])) {
+                    count++;
                 }
-
-                result += intersectionSet.size();
             }
-
-            appendPrefix(i, words[i], &prefixRoot);
-            appendSuffix(i, words[i], &suffixRoot);
         }
 
-        return result;
+        return count;
     }
 };
