887adb1bc4784f146ca891fa4656b0ad9d473d8c
lklab <alexeibatic@gmail.com>
2025-01-10 10:31:46 +0900

    Time: 139 ms (23.75%), Space: 144.6 MB (23.75%) - LeetHub

--- 0916-word-subsets/0916-word-subsets.cpp
+++ 0916-word-subsets/0916-word-subsets.cpp
@@ -2,14 +2,20 @@ class Solution {
 public:
     vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
         vector<int> freq(26, 0);
+        vector<int> freqExists;
 
         for (string& word : words2) {
             vector<int> counter(26, 0);
+            unordered_set<int> exists;
             for (char c : word) {
                 ++counter[c - 'a'];
+                exists.insert(c - 'a');
             }
 
-            for (int i = 0; i < 26; ++i) {
+            for (int i : exists) {
+                if (freq[i] == 0) {
+                    freqExists.push_back(i);
+                }
                 freq[i] = max(freq[i], counter[i]);
             }
         }
@@ -23,7 +29,7 @@ public:
             }
 
             bool success = true;
-            for (int i = 0; i < 26; ++i) {
+            for (int i : freqExists) {
                 if (counter[i] < freq[i]) {
                     success = false;
                     break;
