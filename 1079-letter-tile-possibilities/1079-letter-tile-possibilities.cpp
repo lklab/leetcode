6eff20e5e2492957aa5ed11c5d27c1e7e7fbccbc
lklab <alexeibatic@gmail.com>
2025-02-17 12:00:28 +0900

    Time: 41 ms (35.72%), Space: 17.9 MB (37.37%) - LeetHub

--- 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
@@ -1,14 +1,14 @@
 class Solution {
 private:
-    void dfs(string& tiles, string& current, unordered_set<string>& letters, vector<bool>& used) {
+    void dfs(string& tiles, string current, unordered_set<string>& letters, vector<bool>& used) {
         letters.insert(current);
 
         for (int i = 0; i < tiles.size(); ++i) {
             if (!used[i]) {
-                string next = current + tiles[i];
+                // string next = current + tiles[i];
                 // if (letters.find(next) == letters.end()) {
                     used[i] = true;
-                    dfs(tiles, next, letters, used);
+                    dfs(tiles, current + tiles[i], letters, used);
                     used[i] = false;
                 // }
             }
@@ -19,8 +19,8 @@ public:
     int numTilePossibilities(string tiles) {
         unordered_set<string> letters;
         vector<bool> used(tiles.size(), false);
-        string current = "";
-        dfs(tiles, current, letters, used);
+        // string current = "";
+        dfs(tiles, "", letters, used);
         return letters.size() - 1;
     }
 };
