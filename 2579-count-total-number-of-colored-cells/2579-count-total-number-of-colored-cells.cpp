bfca95d7a13f928122b3140d3a415adcde1d94f2
lklab <alexeibatic@gmail.com>
2025-03-05 09:15:29 +0900

    Time: 0 ms (100%), Space: 8 MB (31.17%) - LeetHub

--- 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.cpp
+++ 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     long long coloredCells(int n) {
-        if (n == 1) {
-            return 1;
-        }
-
-        --n;
-        return n * (8LL + (n - 1) * 4LL) / 2 + 1;
+        return 2LL * (n - 1) * n + 1;
     }
 };
