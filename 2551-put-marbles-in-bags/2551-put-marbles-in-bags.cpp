186162288c28813556122d0473eecc445be92639
lklab <alexeibatic@gmail.com>
2025-03-31 10:10:10 +0900

    Time: 38 ms (46.84%), Space: 64.3 MB (82.7%) - LeetHub

--- 2551-put-marbles-in-bags/2551-put-marbles-in-bags.cpp
+++ 2551-put-marbles-in-bags/2551-put-marbles-in-bags.cpp
@@ -6,15 +6,17 @@ public:
 
         for (int i = 0; i < n - 1; ++i) {
             costs[i] = weights[i] + weights[i + 1];
+            // cout << costs[i] << endl;
         }
         sort(costs.begin(), costs.end());
 
         long long diff = 0;
         for (int i = 0; i < k - 1; ++i) {
-            diff += weights[n - 2 - i];
+            diff += costs[n - 2 - i];
         }
+        // cout << diff << endl;
         for (int i = 0; i < k - 1; ++i) {
-            diff -= weights[i];
+            diff -= costs[i];
         }
 
         return diff;
